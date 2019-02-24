8594c5f30fa345bacd2f28040c17610997daa53d
João Costa <joaopedrocosta.contato@gmail.com>
2019-02-24 19:15:45 -0300

    - Initial commit with project creation and homepage development

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { CustomProgressBarModule } from './components/custom-progress-bar/custom-progress-bar.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatInputModule, MatFormFieldModule, MatButtonModule, MatCardModule, MatAutocompleteModule, MatProgressSpinnerModule, MatIconModule } from '@angular/material'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { GithubInterceptor } from './interceptors/github.interceptor';
+import { ComparatorComponent } from './pages/comparator/comparator.component';
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComparatorComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+	BrowserAnimationsModule,
+	FlexLayoutModule,
+
+	MatInputModule,
+	MatFormFieldModule,
+	MatButtonModule,
+	MatCardModule,
+	MatAutocompleteModule,
+	MatProgressSpinnerModule,
+	MatIconModule,
+	CustomProgressBarModule,
+	
+	ReactiveFormsModule,
+	HttpClientModule
+  ],
+  providers: [
+	{ provide: HTTP_INTERCEPTORS, useClass: GithubInterceptor, multi: true },
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/compare-request.model.ts
@@ -0,0 +1,3 @@
+export class CompareRequestModel {
+	repository_id: number;
+}
